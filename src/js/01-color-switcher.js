90ef1c57ed8ecf5b353ec5726bcae2425f0a5634
IrynaChernysheva <100154794+IrynaChernysheva@users.noreply.github.com>
2022-09-08 12:51:39 +0300

    01-js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+const elementEl = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+elementEl.btnStart.addEventListener('click', onBtnStart);
+elementEl.btnStop.addEventListener('click', onBtnStop);
+function onBtnStart() {
+  elementEl.btnStart.setAttribute('disabled', 'true');
+  elementEl.btnStop.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  console.log(timerId);
+}
+function onBtnStop() {
+  elementEl.btnStop.setAttribute('disabled', 'true');
+  elementEl.btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+// const timer = {
+//     start() {
+//         const startTime = Date.now();
+
+//         setInterval(() => {
+//             const currentTime = Date.now();
+//             const deltaTime = currentTime - startTime;
+//             const { hours, mins, secs } = getRandomHexColor(deltaTime);
+//             console.log(currentTime);
+//         }, 1000);
+//     }
+
+// }
+// timer.start();
